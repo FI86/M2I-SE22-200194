097e65100778ae411ec20613c5e088a1fb305c0f
Twixy Twixy <twixy@free.fr>
2022-09-22 08:51:19 +0200

    Exercices vides

---
+++ Cours-Exercices/Exercices/TP3_Fichiers_Mini_BDD.py
@@ -0,0 +1,52 @@
+# TP 3
+# Mini Systeme BD 
+# Creer un dictionnaire permettant d'enregistrer nom, age et taille d'utilisateurs dans un dictionnaire
+# Creer une fonction inscription pour saisir les données utilisateurs
+# et poser la question si on veut continuer a saisir un utilisateur
+# Creer une fonction consultation qui permet de voir les données utilisateurs enregistrées dans le dictionnaire.
+# Creer un menu pour choisir entre quitter, inscription ou consultation
+
+# Dans un deuxieme temps enregistrer les infos dans un fichier nommé utilisateurs.txt
+# et lisez le fichier lors des consultations.
+# On utilisera le caractère séparateur @ pour séparer la clé de la valeur d'un dictionnaire
+# et le caractère # pour séparer les données constituant cette valeur.
+# Exemple Juliette@18#1.68
+
+# Dans un troisième temps améliorer le script en créant le menu suivant à l'aide d'un dictionnaire
+# qui servira aussi à appler les fonctions correspondantes.
+# (L) Lecture
+# (I) Inscription 
+# (C) Consultation par nom
+# (T) Consultation totale
+# (E) Enregistrement
+# (Q) Quitter
+
+def inscription():
+    pass
+
+def consultation():
+    pass
+
+def consultationTotale():
+    pass
+
+def ligne():
+    pass
+
+def enregistrement():
+    pass
+
+def lecture():
+    pass
+
+def quitter():
+    pass
+
+def clear():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
